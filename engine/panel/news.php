7bd81b8bc85e1e607a15d27d4be142156da21c3a
DeadZeta <36980503+DeadZeta@users.noreply.github.com>
2019-11-14 22:11:52 +0300

    FIX

--- engine/panel/news.php
+++ engine/panel/news.php
@@ -9,7 +9,7 @@ mysqli_select_db($mysqli, $dbconfig['base']) or exit(error("select-base"));
 if(isset($_POST['create-go'])){
    $result = mysqli_fetch_row(mysqli_query($mysqli, "SELECT MAX(id) FROM `core_news`"));
    $createid = $result[0]+'1';
-   mysqli_query($mysqli, "INSERT INTO `core_news`(`id`, `new`) VALUES ('0','".$_POST['create-text']."')");
+   mysqli_query($mysqli, "INSERT INTO `core_news`(`id`, `new`) VALUES ('".$createid."','".$_POST['create-text']."')");
 	if(isset($_FILES) && $_FILES['img']['error'] == 'UPLOAD_ERR_OK'){ // Проверяем, загрузил ли пользователь файл
 $destiation_dir = "$_SERVER[DOCUMENT_ROOT]/upload/news/".$createid.".png"; // Директория для размещения файла
 move_uploaded_file($_FILES['img']['tmp_name'], $destiation_dir ); // Перемещаем файл в желаемую директорию
