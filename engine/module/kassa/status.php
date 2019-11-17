5aac55c8336918b9e377cc1d8c8e4247d9dbcdae
DeadZeta <36980503+DeadZeta@users.noreply.github.com>
2019-11-18 00:00:07 +0300

    Release 2.4

--- engine/module/kassa/status.php
+++ engine/module/kassa/status.php
@@ -21,6 +21,7 @@ $mysqli = mysqli_connect($dbconfig['host'], $dbconfig['user'], $dbconfig['passwo
  if($query->num_rows > 0){
   mysqli_query($mysqli, "UPDATE `core_users` SET `cash`=`cash`+".$_POST['ik_am']." WHERE `name`='".$_POST['ik_x_login']."'");
   mysqli_query($mysqli, "INSERT INTO `core_payments`(`name`, `cash`, `date`, `id`) VALUES ('".$_POST['ik_x_login']."', '".$_POST['ik_am']."', '".time()."', '".$_POST['ik_pm_no']."')");
+  echo "success";
   unset($query);
   unset($_POST);
   unset($sign);
